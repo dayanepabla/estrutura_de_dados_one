04db501df75a5c64aa1257e56e8c2787f66b2490
Dayane Pabla <dayanepabla@gmail.com>
2015-02-08 23:10:05 -0300

    Mudança de variáveis

--- lista00/intervalos.cpp
+++ lista00/intervalos.cpp
@@ -4,34 +4,34 @@ using namespace std;
 
 int main () {
     int x;
-    int cont1 = 0;
-    int cont2 = 0;
-    int cont3 = 0;
-    int cont4 = 0;
-    int cont_total = 0;
+    int counter_interval_one = 0;
+    int counter_interval_two = 0;
+    int counter_interval_three = 0;
+    int counter_interval_four = 0;
+    int counter_total = 0;
 
-    cout <<"Entre com valores inteiros ( Ctrl + d p / encerrar ): " <<endl;
+    cout << "Entre com valores inteiros ( Ctrl + d p / encerrar ): " << endl;
 
     while (cin >> x) {
         if (x >= 0 && x < 25){
-            cont1++;
+            counter_interval_one++;
         }
         if (x >= 25 && x < 50){
-            cont2++;
+            counter_interval_two++;
         }
         if (x >= 50 && x < 75){
-            cont3++;
+            counter_interval_three++;
         }
         if (x >= 75 && x <= 100){
-            cont4++;
+            counter_interval_four++;
         }
-        cont_total++;
+        counter_total++;
     }
 
-    cout <<"Porcentagem de números no intervalo [0, 25): "<<100*cont1/cont_total<<endl;
-    cout <<"Porcentagem de números no intervalo [25, 50): "<<100*cont2/cont_total<<endl;
-    cout <<"Porcentagem de números no intervalo [50, 75): "<<100*cont3/cont_total<<endl;
-    cout <<"Porcentagem de números no intervalo [75, 100]: "<<100*cont4/cont_total<<endl;
+    cout << "Porcentagem de números no intervalo [0, 25): " << 100*counter_interval_one/counter_total <<endl;
+    cout << "Porcentagem de números no intervalo [25, 50): " << 100*counter_interval_two/counter_total <<endl;
+    cout << "Porcentagem de números no intervalo [50, 75): " << 100*counter_interval_three/counter_total <<endl;
+    cout << "Porcentagem de números no intervalo [75, 100]: " << 100*counter_interval_four/counter_total <<endl;
 
     return 0;
 }
