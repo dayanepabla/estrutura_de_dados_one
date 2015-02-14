7f8e2ced1db0978b4d2e2a47cdc0dd7475b5af4b
Dayane Pabla <dayanepabla@gmail.com>
2015-02-13 23:45:25 -0300

    Inicialização da resolução

--- lista00/soma_pares.cpp
+++ lista00/soma_pares.cpp
@@ -5,9 +5,20 @@ using namespace std;
 int main ()
 {
     int m, n;
-    while (cin >> m && cin >> n) {
+    int soma = 0;
+    cout << "Entre com dois valores inteiros ( Ctrl + d para encerrar ): " << endl;
 
+    while ( cin >> m && cin >> n )
+    {
 
+        for ( int i (0); i < n; ++i )
+        {
+            cout << "Entre com dois valores inteiros ( Ctrl + d para encerrar ): " << endl;
+            soma += m + ( n - 1 );
+        }
+
+        cout << "Soma: " << soma << endl;
     }
     return 0;
 }
+
