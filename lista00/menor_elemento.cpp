c974e78f44764398e7c1e9755d24b5d3e7c86694
Dayane Pabla <dayanepabla@gmail.com>
2015-02-16 23:46:03 -0300

    Inicialização da questão

---
+++ lista00/menor_elemento.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float vetor[20];
+    int i;
+    int menor;
+
+    for ( i = 0; i <= 19; i++)
+    {
+
+        cout << "Informe o número " << i + 1 << endl;
+        cin >> vetor[i];
+    }
+
+    cout << "[";
+    for ( i = 0; i <= 19; i++ )
+    {
+
+        cout << vetor[ i ] << " ";
+    }
+
+    cout << "]\n";
+    for (i = 0; i <= 19; i++)
+    {
+        if ( vetor[i] < vetor [i + 1] )
+        {
+            menor = vetor[i];
+            cout << "Menor elemento do vetor: " << menor << endl;
+            cout << "Posição: " << vetor[i] << endl;
+        }
+
+    }
+
+
+    return 0;
+}
