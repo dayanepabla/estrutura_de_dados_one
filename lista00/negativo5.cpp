95c907304750a4c7e86953770379404cec7e9b91
Dayane Pabla <dayanepabla@gmail.com>
2015-02-16 23:10:14 -0300

    Melhorias no código

--- lista00/negativo5.cpp
+++ lista00/negativo5.cpp
@@ -8,13 +8,13 @@ int main() {
     int cont = 0;
 
     while (i <= 5) {
-        cout << "Informe o número "<< i <<endl;
+        cout << "Informe o número " << i << endl;
         cin >> num;
         i++;
         if (num < 0){
             cont++;
         }
     }
-    cout << "Total de números negativos: "<< cont <<endl;
+    cout << "Total de números negativos: " << cont << endl;
     return 0;
 }
