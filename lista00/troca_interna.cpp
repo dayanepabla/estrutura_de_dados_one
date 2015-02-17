6bed318ee4176a4d7dadf8c129e043832ad87a7f
Dayane Pabla <dayanepabla@gmail.com>
2015-02-17 15:00:31 -0300

    Troca do nome do vetor

--- lista00/troca_interna.cpp
+++ lista00/troca_interna.cpp
@@ -4,36 +4,36 @@ using namespace std;
 
 int main()
 {
-    int vetor[20];
+    int A[20];
     int i, aux;
 
     for ( i = 0; i <= 19; i++)
     {
         cout << "Informe o número " << i + 1 << endl;
-        cin >> vetor[i];
+        cin >> A[i];
     }
 
     cout << "[";
 
     for ( i = 0; i <= 19; i++ )
     {
-        cout << vetor[i] << " ";
+        cout << A[i] << " ";
     }
     cout << "]\n";
 
 
     for ( i = 0; i < 10; i++ )
     {
-        aux = vetor[i];
-        vetor[i] = vetor[19 - i];
-        vetor[19 - i] = aux;
+        aux = A[i];
+        A[i] = A[19 - i];
+        A[19 - i] = aux;
     }
 
     cout << "[";
 
     for ( i = 0; i <= 19; i++ )
     {
-        cout << vetor[i] << " ";
+        cout << A[i] << " ";
     }
     cout << "]\n";
 
