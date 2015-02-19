15fe260b6d3442cd56c18a876bc107342db3c022
Dayane Pabla <dayanepabla@gmail.com>
2015-02-19 18:21:48 -0300

    Mudança na lógica da resolução

--- lista00/vet5.cpp
+++ lista00/vet5.cpp
@@ -6,28 +6,31 @@ int main()
 {
     int A[5];
     int B[5];
-    int i;
+    int i, num;
+    int cont = 0;
 
     for ( i = 0; i < 30; i++)
     {
         cout << "Informe o número " << i + 1 << endl;
+        cin >> num;
+        while (cont < 5)
+        {
 
-        if (i % 2 == 0){
-            cin >> A[i];
-        } else {
-            cin >> B[i];
-        }
+            if (num % 2 == 0)
+        {
 
-        if (A[i] == 5){
+            A[i] = num;
             cout << "[";
             cout << A[i] << " ";
             cout << "]\n";
-        }
+        } else {
 
-        if (B[i] == 5){
+            B[i] = num;
             cout << "[";
             cout << B[i] << " ";
             cout << "]\n";
+        }
+            cont++;
         }
 
 
