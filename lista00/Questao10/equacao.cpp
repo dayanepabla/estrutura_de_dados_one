759e6da61abd8bded4b68939be5579e87c10b215
Dayane Pabla <dayanepabla@gmail.com>
2015-02-19 23:42:40 -0300

    Fim do cálculo das raízes

--- lista00/Questao10/equacao.cpp
+++ lista00/Questao10/equacao.cpp
@@ -8,9 +8,18 @@ using namespace std;
 
 int main()
 {
-    float r1, r2;
+    float r1, r2, a, b, c;
 
-    cout << ">> Quantidade de raizes: " << raizes (1.0f, 2.f, 3.f, r1, r2 ) << endl;
+    cout << "Valor para a: ";
+    cin >> a;
+
+    cout << "Valor para b: ";
+    cin >> b;
+
+    cout << "Valor para c: ";
+    cin >> c;
+
+    cout << ">> Quantidade de raizes: " << raizes (a, b, c, &r1, &r2) << endl;
     cout << ">> As raizes sao: " << r1 << " e " << r2 << endl;
 
     return 0;
--- lista00/Questao10/raizes.cpp
+++ lista00/Questao10/raizes.cpp
@@ -1,27 +1,33 @@
 #include <cmath>
-#include "raizes.h"
+#include <iostream>
 
 #define EPS 0.00001
 
-int raizes (float a, float b, float c, float& x1, float& x2) {
+using namespace std;
+
+int raizes (float a, float b, float c, float * x1, float * x2) {
     float delta = 0.f;
 
     delta = b*b - 4.f*a*c;
-    if ( delta < 0.f ) { // Sem raizes reais.
-        x1 = x2 = 0.f;
+
+    if ( delta < 0.f )
+    {
+        *x1 = *x2 = 0.f;
+
         return 0;
-    //} else if( fbas( delta - 0.f ) < EPS ) {
-    } else if( delta > 0.f ) // duas raizes reais.
+    }
+    else if( delta > 0.f )
+    {
+        *x1 = (-b - sqrt(delta)) / 2.f*a;
+        *x2 = (-b + sqrt(delta)) / 2.f*a;
 
         return 2;
     }
-    else { // Apenas 1 raiz real, ou seja, equacao de 1ro grau.
+    else
+    {
+        *x1 = *x2 = (-b + sqrt(delta)) / 2.f*a;
 
         return 1;
     }
-
-    x1 = 5.f;
-    x2 = -sqrt( 25.f );
-    // Codigo....
-    return 2;
 }
+
