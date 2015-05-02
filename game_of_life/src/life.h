467e078f2a2963fd18b7acfcbaea24e3865019d7
Dayane Pabla <dayanepabla@gmail.com>
2015-05-01 22:59:18 -0300

    Initial structure

---
+++ game_of_life/src/life.h
@@ -0,0 +1,19 @@
+/**
+ * @file: life.h
+ * Write description of source file here for dOxygen.
+ *
+ * @brief Can use "brief" tag to explicitly generate comments for file documentation.
+ *
+ */
+#ifndef LIFE_H
+#define LIFE_H
+
+class Life
+{
+  public:
+    Life();
+    ~Life();
+
+};
+
+#endif
---
+++ game_of_life/test/life_test.cpp
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.h"
+
+
+TEST_CASE("its true", "[life]") {
+  REQUIRE(true);
+}
