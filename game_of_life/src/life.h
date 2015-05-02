6b751b77b5b3d6198f03152f82d22274777f0075
Dayane Pabla <dayanepabla@gmail.com>
2015-05-01 23:13:59 -0300

    Fix Makefiles

--- game_of_life/src/life.h
+++ game_of_life/src/life.h
@@ -1,3 +1,6 @@
+#ifndef LIFE_H
+#define LIFE_H
+
 /**
  * @file: life.h
  * Write description of source file here for dOxygen.
@@ -5,15 +8,11 @@
  * @brief Can use "brief" tag to explicitly generate comments for file documentation.
  *
  */
-#ifndef LIFE_H
-#define LIFE_H
-
 class Life
 {
   public:
     Life();
     ~Life();
-
 };
 
 #endif
