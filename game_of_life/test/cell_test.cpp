7f3b5b4b67b9f5d4b84257b751f2bdc4a543998b
Dayane Pabla <dayanepabla@gmail.com>
2015-05-03 10:52:19 -0300

    Atualiza classe Cell para usar Organism

--- game_of_life/test/cell_test.cpp
+++ game_of_life/test/cell_test.cpp
@@ -1,20 +1,104 @@
 #include "catch.h"
 
 #include "cell.h"
+#include "organism.h"
 
-TEST_CASE("a cell initially empty", "[cell]")
+SCENARIO("cells can be killed", "[cell]")
 {
-    Cell cell = Cell();
+    Organism* organism = new Organism(true);
 
-    REQUIRE(cell.empty() == true);
+    REQUIRE(organism->is_alive() == true);
 
-    SECTION("is dead")
+    GIVEN("an empty cell")
     {
-        REQUIRE(cell.dead() == true);
+        Cell cell = Cell();
+
+        REQUIRE(cell.is_empty() == true);
+        REQUIRE(cell.is_dead() == true);
+
+        WHEN("it is killed")
+        {
+            cell.kill();
+
+            THEN("it remains dead")
+            {
+                REQUIRE(cell.is_dead() == true);
+            }
+        }
     }
 
-    SECTION("has no neighbors")
+    GIVEN("a cell initially alive")
     {
-        REQUIRE(cell.neighbors()->size() == 0);
+        Cell cell = Cell(organism);
+
+        REQUIRE(cell.is_alive() == true);
+
+        WHEN("it is killed")
+        {
+            cell.kill();
+
+            THEN("it dies")
+            {
+                REQUIRE(cell.is_dead() == true);
+            }
+        }
     }
+}
+
+SCENARIO("cells can be revived", "[cell]")
+{
+    Organism* organism = new Organism(true);
+
+    REQUIRE(organism->is_alive() == true);
+
+    GIVEN("an empty cell")
+    {
+        Cell cell = Cell();
+
+        REQUIRE(cell.is_empty() == true);
+
+        WHEN("it is revived")
+        {
+            cell.revive(organism);
+
+            THEN("it lives")
+            {
+                REQUIRE(cell.is_alive() == true);
+            }
+        }
+
+        WHEN("it is revived with invalid organism")
+        {
+            cell.revive(nullptr);
+
+            THEN("it remains empty and dead")
+            {
+                REQUIRE(cell.is_empty() == true);
+                REQUIRE(cell.is_dead() == true);
+            }
+        }
+    }
+
+    GIVEN("a non empty cell")
+    {
+        Cell cell = Cell(organism);
+
+        REQUIRE(cell.is_empty() == false);
+        REQUIRE(cell.is_alive() == true);
+
+        WHEN("it is revived")
+        {
+            cell.revive(organism);
+
+            THEN("it remains alive")
+            {
+                REQUIRE(cell.is_alive() == true);
+            }
+        }
+    }
+}
+
+SCENARIO("cells can have neighbors", "[cell]")
+{
+
 }
